62c24db4d071436c406bfb102fd46fdd9848b56d
Southboy12 <menkachi85@gmail.com>
2023-04-23 09:36:42 +0100

    Added a function to transform the data

--- script/covid_etl.py
+++ script/covid_etl.py
@@ -7,14 +7,28 @@ import os
 def extract_data(url) ->pd.DataFrame:
     """download and read the file into a DataFrame"""
 
-    url = "https://drive.google.com/file/d/1SzmRIwlpL5PrFuaUe_1TAcMV0HYHMD_b/view"
     # od.download(url, './data/')
     df = pd.read_csv('./data/covid_19_data.csv')
     return df
 
+def transform(df: pd.DataFrame) -> pd.DataFrame:
+    """Set ObservationDate as datetype and write the transformed date to a csv file"""
+
+    df['ObservationDate'] = pd.to_datetime(df.ObservationDate)
+    print(df.info())
+    print(df.head())
+    return df
+
+def load(df: pd.DataFrame) -> None:
+    """Load the DataFrame into postgresql"""
+
+
+
 
 def main():
-    extract_data()
+    url = "https://drive.google.com/file/d/1SzmRIwlpL5PrFuaUe_1TAcMV0HYHMD_b/view"
+    df = extract_data(url)
+    clean_df = transform(df)
 
 
 
