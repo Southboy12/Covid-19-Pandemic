3404cf23f407527f51c4a924b0aae66ceaf320f0
Southboy12 <menkachi85@gmail.com>
2023-04-24 06:41:22 +0100

    Added comments to the code

--- script/covid_etl.py
+++ script/covid_etl.py
@@ -21,26 +21,43 @@ def get_database_conn():
 def extract_data(url) ->pd.DataFrame:
     """download and read the file into a DataFrame"""
 
-    # od.download(url, './data/')
+    # download the csv file from the url
+    od.download(url, './data/')
+
+    # read the csv file into a DataFrame
     df = pd.read_csv('./data/covid_19_data.csv')
+
+    # print a successful message
     print('Date successfully extracted to csv file')
     return df
 
 def transform(df: pd.DataFrame) -> Path:
     """Set ObservationDate as datetype and write the transformed date to a csv file"""
 
+    # convert datatype of ObservationDate to datetime 
     df['ObservationDate'] = pd.to_datetime(df.ObservationDate) 
+    
+    # save the transformed data as csv to local
     transformed_path = './data/transformed_covid_data.csv'
     df.to_csv(transformed_path, index=False)
+
+    # print a successful message
     print('Data successfully transformed and written to csv file')
     return transformed_path
 
 def load(path: Path) -> None:
     """Load the DataFrame into postgresql"""
 
+    # read the transformed csv from local
     df = pd.read_csv(path)
+
+    # assign database credentials
     engine = get_database_conn()
+
+    # write the data to postgres
     df.to_sql('covid_19_data', con=engine, if_exists='replace', index=False)
+
+    # print a successful message
     print('Data successfully written to postgreSQL database')
 
 def main():
